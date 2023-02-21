c48b5053056554c234c15bfcd659e626812b27c0
jadsonteless <jtcostha@gmail.com>
2023-02-21 12:54:16 -0300

    correção

--- SplitNumber/SplitNumber/Program.cs
+++ SplitNumber/SplitNumber/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; // add System aos Console.
+using System.Globalization; // inslata CultureInfo.InvariantCulture.
+
+namespace split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cod1, cod2, qtd1, qtd2; // var numeros int.
+            double preco1, preco2, total; // var numeros double.
+
+            string[] valores = Console.ReadLine().Split(' '); // recebe e divida as string em partes.
+            cod1 = int.Parse(valores[0]); // add primeira parte.
+            qtd1 = int.Parse(valores[1]); // add segunda parte.
+            preco1 = double.Parse(valores[2], CultureInfo.InvariantCulture); // add terceira parte.
+
+            valores = Console.ReadLine().Split(' '); // recebe segundo valores.
+            cod2 = int.Parse(valores[0]);
+            qtd2 = int.Parse(valores[1]);
+            preco2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            total = preco1 * qtd1 + preco2 * qtd2; // realiza o calculo.
+
+            // imprimi o resultado:
+            Console.WriteLine($"VALOR A PAGAR: R$ {total.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
