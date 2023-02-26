fddde1ceaaee6e50e46f850fc038915ff8b4dae2
jadsonteless <jtcostha@gmail.com>
2023-02-26 15:34:59 -0300

    utilizando MOD % resrto da divisao

---
+++ 1_1_ModResto/1_1_ModResto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uri1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seg, hora, resto, minutos, segundos;
+
+            seg = int.Parse(Console.ReadLine());
+
+            hora = seg / 3600; // realiza divisao dos segundos por 1h em segundos que é igual a 3600seg.
+            resto = seg % 3600; // utiliza o resto que sobra da divisao do segundos por hora em segundos.
+
+            minutos = resto / 60; // divide o resto do segundos por hora em minutos.
+            segundos = resto % 60; // recebe o resto que sobra da divisao acima.
+
+            Console.WriteLine($"{hora}:{minutos}:{seg}");
+            Console.ReadLine();
+        }
+    }
+}
