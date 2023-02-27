3bd796c9abbbfe30ed88e5cb57dfce0964b95483
jadsonteless <jtcostha@gmail.com>
2023-02-26 21:37:34 -0300

    ler total de dias e escreve em anos, meses e dias

---
+++ 1_1_ModRestoC/1_1_ModRestoC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace uri1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, resto, ano, mes, dia;
+
+            N = int.Parse(Console.ReadLine());
+
+            ano = N / 365;
+            resto = N % 365;
+            mes = resto / 30;
+            dia = resto % 30;
+
+            Console.WriteLine($"{ano} ano(s)");
+            Console.WriteLine($"{mes} mes(es)");
+            Console.WriteLine($"{dia} dia(s)");
+            Console.ReadLine();
+        }
+    }
+}
