84ecf598ce5b96822e7f35c24a0acfe4e65b2853
jadsonteless <jtcostha@gmail.com>
2023-02-27 00:05:57 -0300

    estrutura composta

---
+++ 4_0_ifElseEsComposta/4_0_ifElseEsComposta/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EstruturaComposta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas;
+
+            Console.WriteLine("Que horas são?");
+            horas = int.Parse(Console.ReadLine());
+
+
+            // Quando o if vem acompanhado do else é chamado de estrutura composta.
+            if (horas <= 12)
+            {
+                Console.WriteLine("Bom Dias!");
+            }
+            else
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            // if == se e else == se nao
+            // se o if for falso vai executar o bloco de comandos do else.
+
+            Console.ReadLine();
+        }
+    }
+}
