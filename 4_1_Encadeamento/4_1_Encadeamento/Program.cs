f4300f4f7d6a70325baa37e4f94df5f0b1e4e72c
jadsonteless <jtcostha@gmail.com>
2023-02-27 21:43:52 -0300

    if dentro de else = encadeamento

---
+++ 4_1_Encadeamento/4_1_Encadeamento/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EstruturaComposta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas;
+
+            Console.WriteLine("Que horas são?");
+            horas = int.Parse(Console.ReadLine());
+
+
+            // Quando o if vem acompanhado do else é chamado de estrutura composta.
+            if (horas <= 12)
+            {
+                Console.WriteLine("Bom Dias!");
+            }
+            else
+            {
+                if (horas <= 18)
+                {
+                    Console.WriteLine("Boa Tarde!");
+                }
+                else
+                {
+                    Console.WriteLine("Boa Noite!");
+                }
+                
+            }
+            // esse estrutura de condicional é chamanda de encadeamento.
+
+            Console.ReadLine();
+        }
+    }
+}
