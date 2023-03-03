42c6f4311a64b10fa59b63f8fc79c3db666f9524
jadsonteless <jtcostha@gmail.com>
2023-03-03 19:02:53 -0300

    #Estrutura_de_repeticao_enquanto

---
+++ 5_0_WhileEnquantoRepeticao/5_0_WhileEnquantoRepeticao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, soma;
+
+            x = int.Parse(Console.ReadLine());
+
+            soma = 0;
+
+            while (x != 0)
+            {
+                soma = soma + x;
+                Console.WriteLine($"Soma dos itens = {soma}");
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.ReadLine();
+        }
+    }
+}
